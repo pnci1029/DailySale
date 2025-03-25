a6f29cc7029e6a9af3b8303701520d3669da4e71
chhong <pnci1029@gmail.com>
2025-03-25 20:35:51 +0900

    Initial commit

---
+++ src/main/kotlin/com/pro/dailysale/DailySaleApplication.kt
@@ -0,0 +1,11 @@
+package com.pro.dailysale
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DailySaleApplication
+
+fun main(args: Array<String>) {
+    runApplication<DailySaleApplication>(*args)
+}
---
+++ src/test/kotlin/com/pro/dailysale/DailySaleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pro.dailysale
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DailySaleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
