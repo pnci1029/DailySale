1041fcaadf86587253898a1d34c15cf6ad0b3b34
chhong <pnci1029@gmail.com>
2025-03-28 21:57:33 +0900

    구글로그인 3

---
+++ src/main/kotlin/com/pro/dailysale/domain/user/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.pro.dailysale.domain.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByUserEmail(userEmail: String): User?
+}
