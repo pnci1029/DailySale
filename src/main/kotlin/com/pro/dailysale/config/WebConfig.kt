d1b1870cfe6162756f39c98025d6adfe5975fb18
chhong <pnci1029@gmail.com>
2025-03-28 21:56:37 +0900

    웹컨피그

---
+++ src/main/kotlin/com/pro/dailysale/config/WebConfig.kt
@@ -0,0 +1,22 @@
+package com.pro.dailysale.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    @Bean
+    fun restTemplate(): RestTemplate = RestTemplate()
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
