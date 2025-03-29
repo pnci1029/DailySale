42678340532e31351910069e507540f1662f0c28
chhong <pnci1029@gmail.com>
2025-03-29 12:51:08 +0900

    유저정보 코드정리

--- src/main/kotlin/com/pro/dailysale/service/auth/SignupService.kt
+++ src/main/kotlin/com/pro/dailysale/service/auth/SignupService.kt
@@ -1,25 +1,21 @@
 package com.pro.dailysale.service.auth
 
+import com.pro.dailysale.config.JwtTokenProvider
 import com.pro.dailysale.domain.user.User
 import com.pro.dailysale.domain.user.UserRepository
 import com.pro.dailysale.domain.user.enums.UserRole
 import com.pro.dailysale.service.auth.dto.OauthUserInfo
 import com.pro.dailysale.service.auth.dto.TokenResponseDTO
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.net.URLEncoder
-import java.util.Date
 
 @Service
 class SignupService (
     val userRepository: UserRepository,
-    @Value("\${jwt.ACCESS_TOKEN_EXPIRED}") private val accessTokenValidTime: Int,
-    @Value("\${jwt.REFRESH_TOKEN_EXPIRED}") private val refreshTokenValidTime: Int,
-    @Value("\${jwt.secret}") private val secretKey: String,
+    private val jwtTokenProvider: JwtTokenProvider,
     @Value("\${target.origins}") private val origin: String,
 ){
 
@@ -46,36 +42,7 @@ class SignupService (
 
     @Transactional
     fun generateToken(user: User): TokenResponseDTO {
-        val now = Date()
-        val accessTokenExpiration = Date(now.time + accessTokenValidTime)
-        val refreshTokenExpiration = Date(now.time + refreshTokenValidTime)
-
-        // Access Token 생성
-        val accessToken = Jwts.builder()
-            .setSubject(user.userEmail)
-            .claim("id", user.id)
-            .claim("role", user.role.name)
-            .setIssuedAt(now)
-            .setExpiration(accessTokenExpiration)
-            .signWith(SignatureAlgorithm.HS256, secretKey)
-            .compact()
-
-        // Refresh Token 생성
-        val refreshToken = Jwts.builder()
-            .setSubject(user.userEmail)
-            .setIssuedAt(now)
-            .setExpiration(refreshTokenExpiration)
-            .signWith(SignatureAlgorithm.HS256, secretKey)
-            .compact()
-
-        // Refresh Token 저장 (DB 또는 Redis)
-        saveRefreshToken(user.userEmail, refreshToken)
-
-        return TokenResponseDTO(
-            accessToken = accessToken,
-            refreshToken = refreshToken,
-            accessTokenExpiresIn = accessTokenExpiration.time
-        )
+        return jwtTokenProvider.createTokens(user)
     }
 
     fun loginSuccess(jwtToken: TokenResponseDTO, response: HttpServletResponse) {
@@ -89,14 +56,5 @@ class SignupService (
         }
         val authUrl = "$origin/success?$queryString"
         response.sendRedirect(authUrl)
-    }
-
-    // Refresh Token을 저장하는 메서드
-    private fun saveRefreshToken(userEmail: String, refreshToken: String) {
-//        val user = userRepository.findByUserEmail(userEmail)
-//        user?.let {
-//            it.refreshToken = refreshToken
-//            userRepository.save(it)
-//        }
     }
 }
