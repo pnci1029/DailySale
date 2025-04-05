6cf04c83d6a116e46a560dbed6d1a1e2a7eb0eb0
chhong <pnci1029@gmail.com>
2025-04-05 11:33:27 +0900

    리스트 조회 API

--- src/main/kotlin/com/pro/dailysale/newsletter/controller/NewsLetterController.kt
+++ src/main/kotlin/com/pro/dailysale/newsletter/controller/NewsLetterController.kt
@@ -2,10 +2,10 @@ package com.pro.dailysale.newsletter.controller
 
 import com.pro.dailysale.newsletter.controller.dto.NewsLetterPostDTO
 import com.pro.dailysale.newsletter.service.NewsLetterService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/news")
@@ -16,4 +16,15 @@ class NewsLetterController (
     fun createNewsLetter(
         @RequestBody dto: NewsLetterPostDTO
     ) = newsLetterService.createNewsLetter(dto)
+
+    @GetMapping
+    fun getNewsLetters(
+        @RequestParam(required = false) query: String?,
+        @PageableDefault(
+            sort = ["createAt"],
+            direction = Sort.Direction.DESC,
+            size = 7,
+            page = 0
+        ) pageable: Pageable,
+    ) = newsLetterService.getNewsLetters(pageable, query)
 }
--- src/main/kotlin/com/pro/dailysale/newsletter/service/NewsLetterService.kt
+++ src/main/kotlin/com/pro/dailysale/newsletter/service/NewsLetterService.kt
@@ -4,32 +4,35 @@ import com.pro.dailysale.newsletter.controller.dto.NewsLetterPostDTO
 import com.pro.dailysale.newsletter.domain.NewsLetter
 import com.pro.dailysale.newsletter.domain.NewsLetterRepository
 import com.pro.dailysale.util.SecurityUtil.Companion.getCurrentUser
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class NewsLetterService(
     val newsLetterRepository: NewsLetterRepository
 ) {
+
+    @Transactional
     fun createNewsLetter(
         dto: NewsLetterPostDTO
-    ) {
+    ) = getCurrentUser()
+        .let {
+            NewsLetter(
+                title = dto.title,
+                content = dto.content,
+                isSent = "N",
+                sentAt = dto.sentAt
+            )
+        }
+        .let { newsLetterRepository.save(it) }
+        .also { ResponseEntity.ok() }
 
-        val user = getCurrentUser()
-        NewsLetter(
-            title = dto.title,
-            content = dto.content,
-            isSent = "N",
-            sentAt = dto.sentAt
-        )
-//            .also { newsLetterRepository.save(it) }
-            .also { ResponseEntity.ok() }
-    }
-//    ) = NewsLetter(
-//        title = dto.title,
-//        content = dto.content,
-//        isSent = "N",
-//        sentAt = dto.sentAt)
-//        .also { newsLetterRepository.save(it) }
-//        .also { ResponseEntity.ok() }
+    @Transactional(readOnly = true)
+    fun getNewsLetters(
+        pageable: Pageable,
+        query: String?,
+    ) = getCurrentUser()
+        .let { newsLetterRepository.searchNewsLetter(pageable, query) }
 }
