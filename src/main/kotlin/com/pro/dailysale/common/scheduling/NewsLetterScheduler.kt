a9ce39c6d42fb4093aeb48fa74298b7c0c8758c2
chhong <pnci1029@gmail.com>
2025-04-07 20:26:47 +0900

    스케줄링 세팅

--- src/main/kotlin/com/pro/dailysale/common/scheduling/NewsLetterScheduler.kt
+++ src/main/kotlin/com/pro/dailysale/common/scheduling/NewsLetterScheduler.kt
@@ -4,14 +4,17 @@ import com.pro.dailysale.newsletter.domain.NewsLetterRepository
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 
 @Component
 class NewsLetterScheduler(
     val newsLetterRepository: NewsLetterRepository
 ) {
-    @Scheduled(cron = "0 0 12 * * * ") // at 12:00
-    @Transactional
-    fun sendMail() {
 
+//    @Scheduled(cron = "0 0 10 * * * ") // at 12:00
+    @Transactional
+    @Scheduled(cron = "*/10 * * * * *")
+    fun sendMail() {
+        val todayNewLetters = newsLetterRepository.findTodayNewsLetter(LocalDate.now().atStartOfDay())
     }
 }
