67569b5d3b0758c444cfd69dc8125654a9c2b668
chhong <pnci1029@gmail.com>
2025-04-08 22:09:41 +0900

    스케줄링

--- src/main/kotlin/com/pro/dailysale/common/scheduling/NewsLetterScheduler.kt
+++ src/main/kotlin/com/pro/dailysale/common/scheduling/NewsLetterScheduler.kt
@@ -1,6 +1,9 @@
 package com.pro.dailysale.common.scheduling
 
 import com.pro.dailysale.newsletter.domain.NewsLetterRepository
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -8,13 +11,30 @@ import java.time.LocalDate
 
 @Component
 class NewsLetterScheduler(
-    val newsLetterRepository: NewsLetterRepository
+    val newsLetterRepository: NewsLetterRepository,
+
+    @Value("\${smtp.email}") private val sender: String,
+
+    private var javaMailSender: JavaMailSender
 ) {
 
-//    @Scheduled(cron = "0 0 10 * * * ") // at 12:00
     @Transactional
-    @Scheduled(cron = "*/10 * * * * *")
+//    @Scheduled(cron = "*/10 * * * * *")
+        @Scheduled(cron = "0 0 10 * * * ") // at 12:00
     fun sendMail() {
         val todayNewLetters = newsLetterRepository.findTodayNewsLetter(LocalDate.now().atStartOfDay())
+
+        val mimeMessage = javaMailSender.createMimeMessage()
+        val helper = MimeMessageHelper(mimeMessage, true, "UTF-8")
+
+        val title = "안녕하세요. 오늘의 할인소식입니다! "
+        val subject = ""
+
+        helper.setFrom(sender)
+        helper.setTo("pnci1029@gmail.com")
+        helper.setSubject(title)
+        helper.setText(subject, true)
+
+        javaMailSender.send(mimeMessage)
     }
 }
