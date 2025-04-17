293b7ca8441ee177c3481fc82b125ecffb37966e
chhong <pnci1029@gmail.com>
2025-04-17 16:47:52 +0900

    밸리데이션 어쩌구

--- src/main/kotlin/com/pro/dailysale/newsletter/controller/dto/NewsLetterPostDTO.kt
+++ src/main/kotlin/com/pro/dailysale/newsletter/controller/dto/NewsLetterPostDTO.kt
@@ -1,10 +1,17 @@
 package com.pro.dailysale.newsletter.controller.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.NotBlank
 import java.time.LocalDateTime
 
 data class NewsLetterPostDTO(
+    @field:NotBlank
+    @field:NotNull
     val title: String,
+
+    @field:NotBlank
+    @field:NotNull
     val content: String,
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     val sentAt: LocalDateTime,
